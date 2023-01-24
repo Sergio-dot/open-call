efcecd78b8d2c929a311ad716c1e9db118612fb2
Sergio-dot <sergio.siclari@gmail.com>
2023-01-24 17:39:26 +0100

    Added support to static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -21,5 +21,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	// enable static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
