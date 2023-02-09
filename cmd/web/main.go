9ed9d29630a858f89bdf0a9267b4aff4f69a145f
Sergio-dot <sergio.siclari@gmail.com>
2023-02-10 00:21:53 +0100

    Added support to .env files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"github.com/Sergio-dot/open-call/internal/server"
 	"log"
+
+	"github.com/Sergio-dot/open-call/internal/server"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
 	// start server
 	if err := server.Run(); err != nil {
 		log.Fatal(err.Error())
