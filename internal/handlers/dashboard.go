58b197c28231d565a6bc239f6e1f406c8577fdef
Sergio-dot <sergio.siclari@gmail.com>
2023-02-10 00:20:26 +0100

    Project clean up

--- internal/handlers/dashboard.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import "github.com/gofiber/fiber/v2"
-
-func Dashboard(ctx *fiber.Ctx) error {
-	return ctx.Render("dashboard", fiber.Map{
-		"PageTitle": "OpenCall - Home",
-	}, "layouts/main")
-}
--- internal/handlers/home.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import "github.com/gofiber/fiber/v2"
-
-func Home(ctx *fiber.Ctx) error {
-	return ctx.Render("index", fiber.Map{
-		"PageTitle": "OpenCall - Home",
-	}, "layouts/main")
-}
